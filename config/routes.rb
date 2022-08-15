df303fb553d33419381106b2f5d9560d76ff3192
raymart-evangelista <raymart.s.evangelista@gmail.com>
2022-08-15 16:22:26 -0700

    remove Main MVC

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,7 @@
 Rails.application.routes.draw do
-  # root "articles#index"
+  root "articles#index"
 
   get "about", to: "about#index"
-
-  root "main#index"
-
   resources :articles do
     resources :comments
   end
